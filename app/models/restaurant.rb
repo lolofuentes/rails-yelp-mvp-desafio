4e9bb3f813f0b6919bfbbc851357b3f9de34387a
LORETOFUENTES <lfuentes.entergrafic@gmail.com>
2021-08-13 17:35:47 -0400

    validaciones

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,7 @@
 class Restaurant < ApplicationRecord
-    has_many :review,dependent: :destroy
+    has_many :reviews,dependent: :destroy
+    validates :name, presence: true
+    validates :address, presence: true
+    validates :rating, inclusion: { in: [0,1,2,3], allow_nil: false }
+
 end
